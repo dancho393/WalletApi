268071a1cf3d3f5b7ad332eb2c467d389737c101
dancho393 <danez7000@gmail.com>
2025-01-24 18:17:38 +0100

    added wallet repository

---
+++ src/main/java/com/projects/webapi/repo/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.projects.webapi.repo;
+
+import com.projects.webapi.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+}
