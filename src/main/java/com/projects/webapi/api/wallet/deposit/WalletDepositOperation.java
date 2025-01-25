2d1f5a1c6c40082d2fbc3560e7b1d47f3d561782
dancho393 <danez7000@gmail.com>
2025-01-25 20:18:06 +0100

    .

---
+++ src/main/java/com/projects/webapi/api/wallet/deposit/WalletDepositOperation.java
@@ -0,0 +1,7 @@
+package com.projects.webapi.api.wallet.deposit;
+
+import com.projects.webapi.api.base.Operation;
+
+public interface WalletDepositOperation
+        extends Operation<WalletDepositRequest, WalletDepositResponse> {
+}
---
+++ src/main/java/com/projects/webapi/api/wallet/withdraw/WalledWithdrawOperation.java
@@ -0,0 +1,7 @@
+package com.projects.webapi.api.wallet.withdraw;
+
+import com.projects.webapi.api.base.Operation;
+
+public interface WalledWithdrawOperation
+        extends Operation<WalletWithdrawRequest, WalletWithdrawResponse> {
+}
